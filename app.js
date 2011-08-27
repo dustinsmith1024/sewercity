76d0ff1399a6579ce0e7c63c92402ee87cac6363
Dustin Smith <dustinsmith1024@hotmail.com>
2011-08-26 22:42:46 -0500

    initial commitment ceremony

---
+++ app.js
@@ -0,0 +1,58 @@
+var sys = require('sys');
+var exec = require("child_process").exec;
+var os = require('os');
+var fs = require('fs');
+var express = require('express');
+var app = express.createServer();
+var io = require('socket.io');
+var _ = require('underscore')._;
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res){
+  console.log("/ requested");
+  fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+      res.send(text);
+  });
+});
+
+var port = parseInt(process.env.PORT) || 3000;
+app.listen(port);
+io = io.listen(app);
+
+io.sockets.on('connection', function(socket){
+  console.log("Server Connection to Socket.io");
+  
+  socket.on('set username', function(name) {
+    socket.set('username', name, function() {
+      console.log("Username set");
+      socket.emit("name set");
+    });
+  });
+
+  socket.on('update score', function(score) {
+      console.log('Score ' + score);
+      socket.broadcast.emit('score-push', score);
+  });
+
+  socket.on('add player', function(player) {
+    socket.broadcast.emit('player add', player);
+  });
+});
+
---
+++ public/javascripts/scorepad.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+
+var ACTIVE_PLAYER=0;
+
+var socket = io.connect('http://localhost:3000/');
+
+socket.on('disconnect', function(){
+  console.log("Server Connection Dropped!");
+});
+
+socket.on('connection', function(){
+  console.log("Server Connection to Socket.io");
+});
+
+
+socket.on('score-push', function(data) {
+    console.log('Received: ', data);
+    $p = $("tr#player-" + data.player);
+    $p.find(".score").text(data.score);
+    $p.data("score", data.score);
+});
+
+socket.on('player add', function(data) {
+  console.log('Recieved: ',  data);
+  $("#player-list").append('<tr id="player-' + data.player + '" data-player="' + data.player + '" data-player-name="' + data.playerName + '" data-score="0" ><td class="playerName">' + data.playerName + '</td><td class="score">' + data.score + '</td><td></td></tr>');
+});
+
+$("#player-actions > li > a").live("click", function(event) {
+  event.preventDefault();
+  if(ACTIVE_PLAYER!=0){
+    var data = $(this).data();
+    console.log(data);
+    var $p = $("#player-list").find("tr#player-" + ACTIVE_PLAYER);
+    console.log($p.data());
+    var score = Number($p.data("score")) + Number(data.value);
+    $p.data("score", score);
+    $p.find(".score").text(score);
+    socket.emit("update score", $p.data() );
+  }else{
+    console.log("NO PLAYER SELECTED!");
+  }
+});
+
+$("a.update-score").click(function(event){
+  event.preventDefault();
+  var $p = $(this).parents("tr");
+  var data = $p.data();
+  data.score += 10;
+  console.log("Sending: ", data);
+  $p.find(".score").text(data.score);
+  socket.emit("update score", data );
+});
+
+$("a.add-player").click(function(event){
+  event.preventDefault();
+  var name = prompt("Player Namer:", "");
+  $pl = $("#player-list");
+  var players = $pl.children().length + 1;
+  $pl.append('<tr id="player-' + players + '" data-player="' + players + '" data-player-name="' + name + '" data-score="0" ><td class="playerName" >' + name + '</td><td class="score">0</td><td></td></tr>');
+  var data = $pl.find("tr:last").data();
+  console.log("Added player: ", data);
+  socket.emit("add player", data);
+
+});
+
+$("#player-list tr").live("click", function(event) {
+  event.preventDefault();
+  if($(this).hasClass("active") === false) {
+    data = $(this).data();
+    ACTIVE_PLAYER = data.player;
+    console.log("clicked: ", data.player);
+    $("#player-list > .active").removeClass("active");
+    $(this).toggleClass("active");
+  }else{
+    $(this).removeClass("active");
+    ACTIVE_PLAYER=0;
+  }
+});
+
+function addPlayer(id, name){
+  
+}
+
+});
